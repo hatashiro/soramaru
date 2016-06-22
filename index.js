43a8bfef124b3dfba6881bc7b0db126528d5c87d
Jun <me@noraesae.net>
2016-06-22 20:36:30 +0900

    Add index script

---
+++ index.js
@@ -0,0 +1,32 @@
+import Koa from 'koa';
+import serve from 'koa-static';
+import mount from 'koa-mount';
+import Tweet from './models/tweet';
+import {getTweets} from './lib/twitter';
+import {get} from 'koa-route';
+
+const app = new Koa();
+
+app.use(get('/', async ctx => {
+  ctx.body = 'hello, world!';
+}));
+
+app.use(get('/tweets', async ctx => {
+  const {from, to} = ctx.query;
+  let tweets = await Tweet.get(from, to);
+
+  if (tweets.length !== 0) {
+    ctx.body = tweets;
+    return;
+  }
+
+  tweets = await getTweets(from, to);
+  await Promise.all(tweets.map(t => Tweet.insert(t)));
+  ctx.body = tweets;
+}));
+
+app.use(mount('/archives', serve(__dirname + '/archives')));
+
+app.listen(3000, () => {
+  console.log('listening to http://localhost:3000');
+});
