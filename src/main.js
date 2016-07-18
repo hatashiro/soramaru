6dd555e036d46d0ef0d2a932125aa1069d5a9d79
Jun <me@noraesae.net>
2016-07-18 22:01:00 +0900

    Add empty Archive and List components and routes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import App from './components/App';
+import Archive from './components/Archive';
+import List from './components/List';
 import Login from './components/Login';
 
 Vue.use(VueResource);
@@ -11,7 +13,13 @@ const Main = Vue.extend({});
 const router = new VueRouter({ history: true });
 
 router.map({
-  '/': { component: App },
+  '/': {
+    component: App,
+    subRoutes: {
+      '/list/:owner/:slug': { name: 'list', component: List },
+      '/archive/:owner/:slug': { name: 'archive', component: Archive },
+    },
+  },
   '/login': { component: Login },
 });
 
